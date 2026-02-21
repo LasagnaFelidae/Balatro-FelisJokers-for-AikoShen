5d0a201d7bf99eba6ee54a139778c0985f20127b
LasagnaFelidae <tabbygarf@tfwno.gf>
2026-02-21 18:05:48 +0100

    o

---
+++ modules/fonts.lua
@@ -0,0 +1,10 @@
+SMODS.Font{
+    key = "Feli5x8",
+    path = "Feli5x8Mono-Regular.ttf",
+    render_scale = 150,
+    TEXT_HEIGHT_SCALE = 0.83,
+    TEXT_OFFSET = {x=2,y=-1},
+    FONTSCALE = 0.1,
+    squish = 0.8,
+    DESCSCALE = 1
+}
